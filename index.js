a356a3b1239def785d33d3af97eb4bba4ee1bb88
NIKHIL SUNIL JAGTAP <55293407+Nikkk1711@users.noreply.github.com>
2025-03-13 10:48:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+    document.getElementById("confirmLogout").addEventListener("click", function () {
+        // Perform logout logic here, e.g., clearing session or redirecting
+        alert("Logged out successfully!");
+        // Redirect to login page
+        window.location.href = "./login.html";
+    });
